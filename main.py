bd464168f366f89a7edf78a26a050292bdea14bb
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-04-26 02:44:18 -0400

    feat: add player spaceship with basic rendering

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -13,6 +15,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
     
         dt = clock.tick(60) / 100
