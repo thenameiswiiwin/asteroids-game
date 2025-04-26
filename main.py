500ba83457148ec3267fc861070ee0390eb80376
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-04-26 05:19:46 -0400

    feat: add collision detection between bullets and asteroids

--- main.py
+++ main.py
@@ -39,6 +39,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
+
         screen.fill("black")
 
         for obj in drawable:
