ccec0eb875e8bc818f04ec936cc020b009e94de5
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-04-26 02:07:25 -0400

    feat: implement 42 limiting with delta time tracking

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+    
+        dt = clock.tick(60) / 100
 
 if __name__ == "__main__":
     main()
